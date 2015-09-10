b619a08e373b02a7bd62fe1e2d98cc7b71b11241
Angel Velásquez <angvp@archlinux.org>
2015-09-10 19:29:35 -0300

    Fix runtravis.sh

--- runtravis.sh
+++ runtravis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 if [ $TRAVIS_PYTHON_VERSION == '2.6' ] && ([ $DJANGO == 'Django<1.8' ] || [ $DJANGO == 'Django<1.9' ]); then 
-   return 0;  
+   return 0 || exit  
 else
     python setup.py test
 fi
