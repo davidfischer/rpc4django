809d317e7b9335a997cc7813019731fc128f811f
Angel Velásquez <angvp@archlinux.org>
2015-09-10 19:32:08 -0300

    change return just exit on runtravis.sh

--- runtravis.sh
+++ runtravis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 if [ $TRAVIS_PYTHON_VERSION == '2.6' ] && ([ $DJANGO == 'Django<1.8' ] || [ $DJANGO == 'Django<1.9' ]); then 
-   return 0 || exit  
+   exit 0  
 else
     python setup.py test
 fi
