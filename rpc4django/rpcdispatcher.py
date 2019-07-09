7412df7ab14e628f04b34d73b87e30013fe9c671
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 14:58:14 -0700

    Django 2.0 compatibility fix

--- rpc4django/rpcdispatcher.py
+++ rpc4django/rpcdispatcher.py
@@ -15,7 +15,11 @@ try:
     from importlib import import_module
 except ImportError:
     from django.utils.importlib import import_module
-from django.core.urlresolvers import get_mod_func
+try:
+    from django.core.urlresolvers import get_mod_func
+except ModuleNotFoundError:
+    #Django 2.0+
+    from django.urls import get_mod_func
 
 try:
     # Python2.x
